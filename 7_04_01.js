c7915a04696969974a34c1c45a204fa064726cd4
birdhui <102664819+birdhui@users.noreply.github.com>
2022-12-12 17:28:52 +0900

    Add files via upload

---
+++ 7_04_01.js
@@ -0,0 +1,2 @@
+document.write("07_04_01.js");
+document.write("<div style='color: blue; font-size: 24px;'>외부 자바스크립트 파일</div>");
---
+++ 7_04_02.js
@@ -0,0 +1,2 @@
+document.write("07_04_02.js");
+document.write("<div style='color: blue; font-size: 20px;'>외부 자바스크립트 파일</div>");
